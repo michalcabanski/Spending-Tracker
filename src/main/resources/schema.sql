d91b245085160efc74e61c7f9697bdd4dc2af734
Michał Cabański <michalcabanski@wp.pl>
2023-03-05 14:55:08 +0100

    Add schema.sql

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,14 @@
+CREATE TABLE category (
+  id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(45) NOT NULL
+);
+
+CREATE TABLE operation (
+  id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  category_id BIGINT NOT NULL,
+  amount DECIMAL(6,2) NOT NULL,
+  date DATETIME NOT NULL,
+  description VARCHAR(45) NULL
+);
+
+ALTER TABLE operation ADD CONSTRAINT operation_category_id FOREIGN KEY (category_id) REFERENCES category(id);
