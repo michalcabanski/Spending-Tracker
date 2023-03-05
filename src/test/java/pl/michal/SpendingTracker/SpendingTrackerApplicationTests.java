cd20fd809155b3751ee08774553c42ac77a318e0
Michał Cabański <michalcabanski@wp.pl>
2023-03-05 11:41:07 +0100

    Add Spring to project

---
+++ src/test/java/pl/michal/SpendingTracker/SpendingTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.michal.SpendingTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpendingTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
