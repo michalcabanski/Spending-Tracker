642fc53eca3d3f876a5ea8d0c86e340eee19e463
Michał Cabański <michalcabanski@wp.pl>
2023-04-11 20:31:56 +0200

    Add more data to data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,38 @@
-INSERT INTO category (name) VALUES ('Salary'), ('Groceries'), ('Restaurant'), ('Rent'), ('Fuel');
+INSERT INTO category (name) VALUES ('Salary'),
+('Groceries'),
+('Restaurant'),
+('Rent'),
+('Fuel'),
+('Subscription'),
+('Clothes'),
+('Public transport');
 
-INSERT INTO operation (category_id, amount, created, description) VALUES (1, 5350.00, '2023-03-01', '');
-INSERT INTO operation (category_id, amount, created, description) VALUES (2, -23.56, '2023-03-02', 'Lidl');
-INSERT INTO operation (category_id, amount, created, description) VALUES (3, -123.56, '2023-03-03', 'Surfburger');
-INSERT INTO operation (category_id, amount, created, description) VALUES (1, -1300.00, '2023-03-04', '');
-INSERT INTO operation (category_id, amount, created, description) VALUES (1, -455.69, '2023-03-05', '');
+INSERT INTO operation (category_id, amount, created, description) VALUES (1, 5000.00, '2023-01-01', '');
+INSERT INTO operation (category_id, amount, created, description) VALUES (1, 5000.00, '2023-02-01', '');
+INSERT INTO operation (category_id, amount, created, description) VALUES (1, 5000.00, '2023-03-01', '');
+INSERT INTO operation (category_id, amount, created, description) VALUES (2, -123.56, '2023-01-03', 'Lidl');
+INSERT INTO operation (category_id, amount, created, description) VALUES (2, -181.64, '2023-01-12', 'Tesco');
+INSERT INTO operation (category_id, amount, created, description) VALUES (2, -56.68, '2023-01-17', 'Lidl');
+INSERT INTO operation (category_id, amount, created, description) VALUES (2, -24.52, '2023-01-24', 'Spar');
+INSERT INTO operation (category_id, amount, created, description) VALUES (2, -134.57, '2023-01-04', 'Lidl');
+INSERT INTO operation (category_id, amount, created, description) VALUES (2, -203.19, '2023-02-11', 'Tesco');
+INSERT INTO operation (category_id, amount, created, description) VALUES (2, -83.56, '2023-02-19', 'Lidl');
+INSERT INTO operation (category_id, amount, created, description) VALUES (2, -74.67, '2023-02-28', 'Lidl');
+INSERT INTO operation (category_id, amount, created, description) VALUES (2, -87.60, '2023-02-06', 'Lidl');
+INSERT INTO operation (category_id, amount, created, description) VALUES (2, -42.35, '2023-02-14', 'Spar');
+INSERT INTO operation (category_id, amount, created, description) VALUES (2, -154.06, '2023-02-23', 'Lidl');
+INSERT INTO operation (category_id, amount, created, description) VALUES (3, -39.99, '2023-01-18', 'Surfburger');
+INSERT INTO operation (category_id, amount, created, description) VALUES (3, -45.00, '2023-02-14', 'Pizza');
+INSERT INTO operation (category_id, amount, created, description) VALUES (4, -2000.00, '2023-01-03', '');
+INSERT INTO operation (category_id, amount, created, description) VALUES (4, -2000.00, '2023-02-02', '');
+INSERT INTO operation (category_id, amount, created, description) VALUES (4, -2000.00, '2023-03-06', '');
+INSERT INTO operation (category_id, amount, created, description) VALUES (5, -400.00, '2023-01-02', '');
+INSERT INTO operation (category_id, amount, created, description) VALUES (5, -355.00, '2023-03-05', '');
+INSERT INTO operation (category_id, amount, created, description) VALUES (6, -60.00, '2023-01-18', 'Netflix');
+INSERT INTO operation (category_id, amount, created, description) VALUES (6, -60.00, '2023-02-18', 'Netflix');
+INSERT INTO operation (category_id, amount, created, description) VALUES (6, -60.00, '2023-03-18', 'Netflix');
+INSERT INTO operation (category_id, amount, created, description) VALUES (7, -159.00, '2023-01-12', 'Adidas');
+INSERT INTO operation (category_id, amount, created, description) VALUES (7, -359.00, '2023-03-02', 'Nike');
+INSERT INTO operation (category_id, amount, created, description) VALUES (8, -80.00, '2023-01-09', '');
+INSERT INTO operation (category_id, amount, created, description) VALUES (8, -80.00, '2023-02-09', '');
+INSERT INTO operation (category_id, amount, created, description) VALUES (8, -80.00, '2023-03-09', '');
